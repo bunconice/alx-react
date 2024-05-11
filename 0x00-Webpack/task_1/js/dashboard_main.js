5a59a9075dc74222d76fc82959d1b66a0e124f4d
Bunmi Akinola <bunconice@gmail.com>
2024-05-11 13:21:19 +0100

    updated

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -5,7 +5,7 @@ let counter = 0;
 
 $('body').append("<p>Holberton Dashboard</p>");
 $('body').append("<p>Dashboard data for the students</p>");
-$('bocy').append("<button>Click here to get started</button>");
+$('body').append("<button>Click here to get started</button>");
 $('body').append("<p id='count'></p>");
 $('body').append("<p>Copyright - Holberton School</p>");
 
