eff93d202d2fae9f7f5e690506e96b91b07adc04
Bunmi Akinola <bunconice@gmail.com>
2024-05-24 15:51:28 +0100

    updated

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,31 +1,34 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   entry: path.resolve(__dirname, './js/dashboard_main.js'),
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
-  },
   performance: {
-		maxAssetSize: 1000000,
-    maxEntrypointSize: 1000000,
-	},
+    maxAssetSize: 1000000,
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(jpe?g|svg|png|gif)$/i,
-        type: 'asset/resource',
+        test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
+          "file-loader",
           {
-            loader: 'file-loader'
-          }
-        ]
-      }
-    ]
-  }
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
